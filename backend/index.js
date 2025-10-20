2919a4fc49737d7c4d217678549d616d122f4b2c
KX <kenxhen@gmail.com>
2025-10-20 12:48:35 +0800

    Added PORT to backend

--- backend/index.js
+++ backend/index.js
@@ -45,4 +45,8 @@ app.post('/transactions', (req, res) => {
 })
 
 
-app.listen(3000)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
